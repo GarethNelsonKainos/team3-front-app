edfba020501ae6c8a71e24b32fa7970f3f786edb
kainos-nialloneill <niall.o'neill@kainos.com>
2026-02-10 13:13:57 +0000

    register jobRoleController to handle /job-roles route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
+
 import express, { Request, Response } from "express";
 import nunjucks from "nunjucks";
 import jobRoleController from "./controllers/jobRoleController";
 
+
 const app = express();
+
+
+app.use(express.static("public"));
+app.use(jobRoleController);
 
 nunjucks.configure("templates", {
   autoescape: true,
   express: app,
 });
-
-app.use(jobRoleController);
 
 app.get("/", (req: Request, res: Response) => {
   res.render("index.html");
