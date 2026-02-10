cb18cd479977db6e48eb14cfb16447001321f4b7
neilobriaink <neil.obriain@kainos.com>
2026-02-10 14:42:19 +0000

    Fix format and lint errors

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import nunjucks from "nunjucks";
 const app = express();
 
 nunjucks.configure("templates", {
-  autoescape: true,
-  express: app,
+	autoescape: true,
+	express: app,
 });
 
-app.get("/", (req: Request, res: Response) => {
-  res.render("index.html");
+app.get("/", (_req: Request, res: Response) => {
+	res.render("index.html");
 });
 
 app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+	console.log("Server running on http://localhost:3000");
 });
