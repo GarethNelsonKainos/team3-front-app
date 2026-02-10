ed59704733eeee1a15cd57e117d77f6b0119d38c
neilobriaink <neil.obriain@kainos.com>
2026-02-10 15:14:54 +0000

    Change format to check, add build

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from "vitest";
 import express from "express";
+import { describe, expect, it } from "vitest";
 
 describe("Express Server", () => {
 	it("should create an express app", () => {
