742222669ea45dab1dce79c710abdcf2fc167f95
kainos-nialloneill <niall.o'neill@kainos.com>
2026-02-11 13:12:01 +0000

    fixed lint issue and updated biome config

--- src/controllers/jobRoleController.ts
+++ src/controllers/jobRoleController.ts
@@ -3,7 +3,7 @@ import jobRoleService from "../services/jobRoleService";
 
 const router = Router();
 
-router.get("/job-roles", async (req: Request, res: Response) => {
+router.get("/job-roles", async (_req: Request, res: Response) => {
 	try {
 		const roles = await jobRoleService.getOpenJobRoles();
 		res.render("job-role-list.html", { roles });
