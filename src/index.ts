f2ae93561f94fb2c44ad1800d3ff44e33ec8cbc1
neilobriaink <neil.obriain@kainos.com>
2026-02-11 16:12:58 +0000

    Make separate page for login

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import nunjucks from "nunjucks";
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 nunjucks.configure("templates", {
 	autoescape: true,
@@ -12,6 +13,16 @@ nunjucks.configure("templates", {
 
 app.get("/", (_req: Request, res: Response) => {
 	res.render("index.html");
+});
+
+app.get("/login", (_req: Request, res: Response) => {
+	res.render("login.html");
+});
+
+app.post("/login", (req: Request, res: Response) => {
+	const { email, password } = req.body as { email?: string; password?: string };
+	console.log("Login attempt", { email, passwordPresent: Boolean(password) });
+	res.status(200).send("Login received");
 });
 
 app.listen(3000, () => {
