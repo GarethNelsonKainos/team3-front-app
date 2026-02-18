dbd0afaeb06027971ad7a9cabd48e528551c8d8b
tom-kainos <thomas.ferguson@kainos.com>
2026-02-18 15:34:58 +0000

    Add the empty line the linter wanted

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -3,12 +3,12 @@ import type FormData from "form-data";
 
 const backendUrl = process.env.API_URL || "http://localhost:3001";
 if (!backendUrl) {
-    throw new Error("API_URL environment variable is not set");
+	throw new Error("API_URL environment variable is not set");
 }
 
 export async function uploadCV(id: string, formData: FormData, token?: string) {
-    const apiUrl = `${backendUrl}/job-roles/${id}/apply`;
-    await axios.post(apiUrl, formData, {
+	const apiUrl = `${backendUrl}/job-roles/${id}/apply`;
+	await axios.post(apiUrl, formData, {
 		headers: {
 			...formData.getHeaders(),
 			...(token ? { Authorization: `Bearer ${token}` } : {}),
