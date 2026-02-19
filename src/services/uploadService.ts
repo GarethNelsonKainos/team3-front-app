641c93568dfd8460d63255eaab30ca4b11b32196
tom-kainos <thomas.ferguson@kainos.com>
2026-02-19 10:29:51 +0000

    remove fallback backend api url

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 import type FormData from "form-data";
 
 const API_BACKEND_URL = process.env.API_BASE_URL || "http://localhost:3001";
-if (!API_BACKEND_URL) {
-	throw new Error("API_BASE_URL environment variable is not set");
-}
 
 export async function uploadCV(id: string, formData: FormData, token?: string) {
 	const apiUrl = `${API_BACKEND_URL}/api/job-roles/${id}/apply`;
