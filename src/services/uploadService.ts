3b64a6aca7c498c69ab6dedb46af507e014c5197
tom-kainos <thomas.ferguson@kainos.com>
2026-02-19 09:46:00 +0000

    Add missing '/api/' from path URL

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import type FormData from "form-data";
 
-const backendUrl = process.env.API_URL || "http://localhost:3001";
-if (!backendUrl) {
-	throw new Error("API_URL environment variable is not set");
+const API_BACKEND_URL = process.env.API_BASE_URL || "http://localhost:3001";
+if (!API_BACKEND_URL) {
+	throw new Error("API_BASE_URL environment variable is not set");
 }
 
 export async function uploadCV(id: string, formData: FormData, token?: string) {
-	const apiUrl = `${backendUrl}/job-roles/${id}/apply`;
+	const apiUrl = `${API_BACKEND_URL}/api/job-roles/${id}/apply`;
 	await axios.post(apiUrl, formData, {
 		headers: {
 			...formData.getHeaders(),
